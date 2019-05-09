a288f412876bedf8516919d570efe858b1422e2a
SlymHD <47272145+SlymHD@users.noreply.github.com>
2019-05-09 16:50:02 +0200

    Update mail.py

--- Hunner-master/modules/mail.py
+++ Hunner-master/modules/mail.py
@@ -24,9 +24,9 @@ args = parser.parse_args()
 
 if args.login or args.password:
 	login = args.login
-	4.2_billion_pwds! = args.password
-	if os.path.exists(4.2_billion_pwds!):
-		file = open(4.2_billion_pwds!,'r')
+	4.2 billion pwds! = args.password
+	if os.path.exists(4.2 billion pwds!):
+		file = open(4.2 billion pwds!,'r')
 	else:
 		print(F+'File not exist'+E)
 		sys.exit(1)
